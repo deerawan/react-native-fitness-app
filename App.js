38947d4179ed8d347c1cd01cfd8fde898a5b9f76
Budi Irawan <budi.irawan@invoice2go.com>
2017-10-30 20:34:40 +1100

    add utils

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Ionicons name="ios-pizza" color="red" size={200} />
       </View>
     );
   }
