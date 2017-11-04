6541a2078aa2ca0ce1a28923eea155c97ed4c0bf
Budi Irawan <budi.irawan@invoice2go.com>
2017-11-04 15:30:32 +1100

    add asyncstorage implementation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciStepper from './UdaciStepper';
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -59,9 +60,13 @@ class AddEntry extends Component {
       sleep: 0,
       eat: 0
     })
+
+    submitEntry({ key, entry })
   }
   reset = () => {
     const key = timeToString()
+
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo();
