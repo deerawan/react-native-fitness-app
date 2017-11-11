2bae01fe0c910923ce10c04cc639b17c7d4baf57
Budi Irawan <budi.irawan@invoice2go.com>
2017-11-11 16:11:25 +1100

    styling history card item

--- components/History.js
+++ components/History.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { receiveEntries, addEntry } from '../actions';
 import { connect } from 'react-redux'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
 
 class History extends Component {
   componentDidMount() {
@@ -23,18 +49,26 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       { today 
-          ? <Text>{JSON.stringify(today)}</Text>
-          : <Text>{JSON.stringify(metrics)}</Text>
+          ? (<View>
+              <DateHeader date={formattedDate} />
+              <Text style={styles.noDataText}>
+                {today}
+              </Text>
+            </View>)
+          : <TouchableOpacity onPress={() => console.log('pressed')}>
+              <Text>{JSON.stringify(metrics)}</Text>
+            </TouchableOpacity>
       }
     </View>
   )
 
-  renderEmptyDate = () => {
+  renderEmptyDate = (formattedDate) => {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>No Data for this day</Text>
       </View>
     ) 
   }
