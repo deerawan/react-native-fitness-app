f8b6c0c3711b82800c92ec66111e0c9633a682f1
Budi Irawan <budi.irawan@invoice2go.com>
2017-11-18 07:43:59 +1100

    UI live component

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator } from 'react-native'
+import { Text, View, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { white, purple } from '../utils/colors'
 
 class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'denied',
     direction: ''
+  }
+  askPermission = () => {
+
   }
   render() {
     const { coords, status, direction } = this.state;
@@ -15,20 +20,64 @@ class Live extends Component {
     }
     
     if (status === 'denied') {
-      return <View><Text>Denied</Text></View>
+      return (
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location service for this app
+          </Text>
+        </View>
+      )
     }
 
-    if (status === 'undeterminded') {
-      return <View><Text>undeterminded</Text></View>
+    if (status === 'undetermined') {
+      return (
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You need to enable location service for this app
+          </Text>
+          <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Live</Text>
         <Text>{JSON.stringify(this.state)}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText :{
+    color: white,
+    fontSize: 20,
+  }
+})
 
 export default Live;
